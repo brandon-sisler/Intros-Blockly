a6f13dc3ccd544afa1e9e79265499e3e60b8aac2
Steven Clontz <steven.clontz@gmail.com>
2023-03-12 23:35:00 +0000

    add spatext as dependency

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import type Blockly from '../lib/blockly';
     import BlocklyComponent from '../lib/Blockly.svelte';
 	import { onMount } from 'svelte';
+    import SpaTeXt from 'spatext';
     import toolbox from '../lib/blockly/toolbox';
     import SpatextGenerator from '../lib/blockly/generators/Spatext'
     import CoqGenerator from '../lib/blockly/generators/Coq'
@@ -10,12 +11,17 @@
 	onMount(async () => {
         workspace.addChangeListener(()=>workspace=workspace);
 	});
+    let stx = "<spatext/>"
+    $: if (workspace) {
+        stx = `<spatext>\n${SpatextGenerator.workspaceToCode(workspace)}\n</spatext>`
+    }
 </script>
 
 <!-- <input type='number' bind:value={height}/> -->
 <BlocklyComponent {height} width={1200} {toolbox} bind:workspace/>
 
 {#if workspace}
-<textarea id="blocklyProof" style="height: 320px; width: 600px;" readonly>{SpatextGenerator.workspaceToCode(workspace)}</textarea>
+<textarea id="blocklyProof" style="height: 320px; width: 600px;" readonly>{stx}</textarea>
 <textarea id="blocklyCoq" style="height: 320px; width: 600px;" readonly>{CoqGenerator.workspaceToCode(workspace)}</textarea>
+<SpaTeXt {stx}/>
 {/if}
